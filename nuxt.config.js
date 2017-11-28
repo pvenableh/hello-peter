4064e79db4246e0cd5ab7812de82c277e0227ed3
pvenableh <peter@huestudios.com>
2017-11-27 22:07:17 -0500

    Testing

---
+++ nuxt.config.js
@@ -0,0 +1,20 @@
+
+module.exports = {
+  router: {
+  },
+  head: {
+    title: 'hue creative agency',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'huestudios.com 4.0' }
+    ],
+    script: [
+      { src: '//fast.fonts.net/jsapi/524a3767-4380-44c1-825f-dfe5af404e7b.js' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'http://huestudios.com/favicon.ico' }
+    ]
+  },
+  loading: { color: '#ffCC00', height: '10px' }
+}
